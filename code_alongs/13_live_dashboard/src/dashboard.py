ac79e93490cefe67cb66ca2a2109003de69ac7ab
Andres <andresgtrujillof@gmail.com>
2025-02-14 13:21:16 +0100

    dashboard fix

--- code_alongs/13_live_dashboard/src/dashboard.py
+++ code_alongs/13_live_dashboard/src/dashboard.py
@@ -1,49 +1,53 @@
 import streamlit as st
-from streamlit_autorefresh import st_autorefresh
-from sqlalchemy import create_engine
-import pandas as pd
 from constants import (
-    POSTGRES_USER,
     POSTGRES_DBNAME,
     POSTGRES_HOST,
     POSTGRES_PASSWORD,
     POSTGRES_PORT,
+    POSTGRES_USER,
 )
-
+from sqlalchemy import create_engine
+import pandas as pd
 from charts import line_chart
+from streamlit_autorefresh import st_autorefresh
 
 
 connection_string = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DBNAME}"
 
-count = st_autorefresh(interval=10 * 1000, limit=100, key="data_refresh")
-
 engine = create_engine(connection_string)
 
+count = st_autorefresh(interval=1000*10, limit=100)
 
 def load_data(query):
     with engine.connect() as conn:
         df = pd.read_sql(query, conn)
-        # print(df)
-        df = df.set_index("timestamp")
     return df
 
 
 def layout():
-    df = load_data(
-        """
-                SELECT * FROM bitcoin;
-                """
-    )
+    df = load_data("SELECT * FROM bitcoin;")
+
     st.markdown("# Bitcoin data")
+
     st.markdown("## Latest data")
-    
+
+    st.markdown("Latest data directly from postgres database in a docker container")
+
     st.dataframe(df.tail())
-    
+
     st.markdown("## Bitcoin latest price in USD")
 
-    price_chart = line_chart(x=df.index, y=df["price_usd"], title="price USD")
+    # plotting code
+    price_chart = line_chart(
+        df["timestamp"],
+        df["price_usd"],
+        title="price USD for bitcoin",
+        xlabel="time",
+        ylabel="USD",
+    )
 
-    st.pyplot(price_chart, bbox_inches="tight")
+    st.pyplot(price_chart)
+
 
 if __name__ == "__main__":
     layout()
